2103e546bf74957ea30743f56ab26079cd3ac348
Nidhi <nidhis2k02@gmail.com>
2024-02-23 09:59:41 +0530

    changes

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ImageCarousel from '../Components/Carousel'
 import PopularPlaces from '../Components/PopularDresses'
 
+
 const Homepage = () => {
   return (
     <div>Homepage
       <PopularPlaces />
       <ImageCarousel />
+     
     </div>
   )
 }
