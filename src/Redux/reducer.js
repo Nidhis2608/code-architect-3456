af33584bb3e837bd67999f58a026502b0edf55d8
Gulshaba <gulshabaparveen1@gmail.com>
2024-02-25 11:36:13 +0530

    kids section

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,20 +1,82 @@
+// import {
+// REGISTER_SUCCESS,  LOGIN_SUCCESS, LOGOUT_SUCCESS, 
+// } from './actionTypes';
+
+// let initialstate = {
+//   isloading: false,
+//   iserror: false,
+//   users: [],
+//   isAuth: false,
+//   token: null,
+//   users:[],
+// }
+
+
+// const reducer = (state = initialstate, action) => {
+//   switch (action.type) {
+ 
+
+//     case REGISTER_SUCCESS:
+//       return { ...state, isloading: false, iserror: false, users: [action.payload] }
+
+//     case LOGIN_SUCCESS:
+//       return { ...state, isloading: false, iserror: false, isAuth: true, token: action.payload }
+
+
+
+// case LOGOUT_SUCCESS:
+//   return { ...state, isloading: false, iserror: false, isAuth: false,token:null }
+
+
+
+
+//     default:
+//       return state;
+//   }
+// };
+
+// export { reducer };
+
+
+
+
+import React from 'react'
 import {
-REGISTER_SUCCESS,  LOGIN_SUCCESS, LOGOUT_SUCCESS, 
+  GET_KIDS_DATA_FAILURE, GET_KIDS_DATA_REQUEST, GET_KIDS_DATA_SUCCESS,
+  GET_KIDS_FAILURE, GET_KIDS_REQUEST, GET_KIDS_SUCCESS, DELETE_KIDS_DATA_REQUEST, DELETE_KIDS_DATA_FAILURE, DELETE_KIDS_DATA_SUCCESS, REGISTER_REQUEST, REGISTER_FAILURE, REGISTER_SUCCESS, LOGIN_REQUEST, LOGIN_FAILURE, LOGIN_SUCCESS, LOGOUT_SUCCESS
 } from './actionTypes';
 
 let initialstate = {
   isloading: false,
   iserror: false,
+  destination: [],
+  mybooking: [],
   users: [],
   isAuth: false,
-  token: null,
-  users:[],
+  token: null
 }
 
 
 const reducer = (state = initialstate, action) => {
   switch (action.type) {
- 
+    // Coffee Data Cases
+    case GET_KIDS_REQUEST:
+      return { ...state, isloading: true };
+    case GET_KIDS_SUCCESS:
+      return { ...state, isloading: false, destination: action.payload };
+    case GET_KIDS_FAILURE:
+      return { ...state, isloading: false, iserror: true };
+
+
+    /////satmrngpraveenchanges
+    case GET_KIDS_DATA_REQUEST || REGISTER_REQUEST || LOGIN_REQUEST:
+      return { ...state, isloading: true }
+
+    case GET_KIDS_DATA_SUCCESS:
+      return { ...state, isloading: false, mybooking: action.payload }
+
+    case GET_KIDS_DATA_FAILURE || REGISTER_FAILURE || LOGIN_FAILURE:
+      return { ...state, isloading: false, iserror: true }
 
     case REGISTER_SUCCESS:
       return { ...state, isloading: false, iserror: false, users: [action.payload] }
@@ -22,6 +84,16 @@ const reducer = (state = initialstate, action) => {
     case LOGIN_SUCCESS:
       return { ...state, isloading: false, iserror: false, isAuth: true, token: action.payload }
 
+
+    case DELETE_KIDS_DATA_REQUEST:
+      return { ...state, isloading: true }
+
+    case DELETE_KIDS_DATA_SUCCESS:
+      const updatedbooking = state.mybooking.filter((booking) => booking.id != action.payload)
+      return { ...state, isloading: false, mybooking: updatedbooking, iserror: false }
+
+    case DELETE_KIDS_DATA_FAILURE:
+      return { ...state, isloading: false, iserror: true }
 
 
 case LOGOUT_SUCCESS:
--- src/Routing/Allroutes.jsx
+++ src/Routing/Allroutes.jsx
@@ -1,18 +1,53 @@
+// import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import Homepage from '../Pages/Homepage';
+// import Loginsignup from '../Pages/signup';
+// import Login from '../Pages/Login';
+// const Allroutes = () => {
+
+
+
+//     return (
+//       <Routes>
+//         <Route path="/" element={<Homepage />} />
+//         <Route path="/signup" element={<Loginsignup />} />
+//       <Route path="/login" element={<Login />} />
+//       </Routes>
+//   );
+// };
+
+// export default Allroutes;
+
+
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Women from '../Pages/Women';
+import Kids from '../Pages/Kids';
+import Men from '../Pages/Men';
+import AboutUs from '../Pages/AboutUs';
 import Homepage from '../Pages/Homepage';
+import  SingleKids  from '../Pages/SingleKids';
+import PrivateRouting from './PrivateRouting';
 import Loginsignup from '../Pages/signup';
 import Login from '../Pages/Login';
 const Allroutes = () => {
 
 
 
-    return (
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/signup" element={<Loginsignup />} />
-      <Route path="/login" element={<Login />} />
-      </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      {/* <Route path="/booking" element={<Bookingpage />} /> */}
+      <Route path="/Women" element={<Women />} />
+      <Route path="/singleKids/:id" element={<PrivateRouting><SingleKids /></PrivateRouting>} />
+      <Route path="/Kids" element={<Kids />} />
+      <Route path="/Men" element={<Men />} />
+      {/* <Route path="/login" element={<Login />} /> */}
+      <Route path="/aboutus" element={<AboutUs />}/>
+      <Route path="/signup" element={<Loginsignup />} />
+//       <Route path="/login" element={<Login />} />
+
+    </Routes>
   );
 };
 
