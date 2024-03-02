6638ebf7c3efeeeba6ac11d1e475815952c2761e
Gulshaba <gulshabaparveen1@gmail.com>
2024-03-03 01:11:43 +0530

    rating check

--- src/Pages/Cartpage.jsx
+++ src/Pages/Cartpage.jsx
@@ -1,93 +1,5 @@
 
 
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { deleteBookingData, fetchBookingsData } from '../Redux/action';
-// import { Box, Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text, Button } from '@chakra-ui/react';
-// import { useNavigate } from 'react-router-dom';
-// import Footer from '../Components/Footer';
-
-// const CartPage = () => {
-//   const dispatch = useDispatch();
-//   const { isloading, iserror, mybooking } = useSelector((state) => state);
-//   console.log(mybooking);
-
-//   useEffect(() => {
-//     dispatch(fetchBookingsData());
-//   }, [dispatch]);
-
-//   const handleDeleteBooking = (id) => {
-//     dispatch(deleteBookingData(id));
-//   };
-
-//   const navigate = useNavigate();
-
-//   const handleNavi = (item) => {
-//     navigate("/payment", { state: { amount: item.price } });
-//   };
-
-//   return (
-//     <div>
-//       Cartpage
-
-//       {isloading ? (
-//         <h3>Loading....</h3>
-//       ) : iserror ? (
-//         <h1>Error loading booking details</h1>
-//       ) : mybooking.length === 0 ? (
-//         <h2>No tours booked yet!</h2>
-//       ) : (
-//         <div>
-//           {mybooking.map((item, index) => (
-//             <div key={item.id} className="booking-card">
-//               <Card
-//                 direction={{ base: 'column', sm: 'row' }}
-//                 overflow='hidden'
-//                 variant='outline'
-//                 style={{
-//                   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-//                   backgroundColor: index % 2 === 0 ? '#EDEDED' : '#FFFFFF',
-//                 }}
-//               >
-//                 <Image
-//                   objectFit='cover'
-//                   maxW={{ base: '100%', sm: '200px' }}
-//                   src={item.image}
-//                   alt='Caffe Latte'
-//                 />
-
-//                 <Stack align="left" justify="center">
-//                   <CardBody>
-//                     <Heading size='md'>{item.location}</Heading>
-//                     <Box py='2'>
-//                       <Text align="left">{item.country}</Text>
-//                     </Box>
-//                     <Box py='2'>
-//                       <Text align="left">Price(INR): {item.price}</Text>
-//                     </Box>
-//                   </CardBody>
-
-//                   <CardFooter>
-//                     <Button onClick={() => handleDeleteBooking(item.id)} variant='solid' colorScheme='teal'>
-//                       DELETE
-//                     </Button>
-//                     <Button onClick={() => handleNavi(item)} variant='solid' colorScheme='blue' ml="2">
-//                       Proceed To Pay
-//                     </Button>
-//                   </CardFooter>
-//                 </Stack>
-//               </Card>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-     
-//     </div>
-//   );
-// };
-
-// export default CartPage;
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteBookingData, fetchBookingsData } from '../Redux/action';
@@ -150,7 +62,7 @@ const CartPage = () => {
                       <Text align="left">{item.country}</Text>
                     </Box>
                     <Box py='2'>
-                      <Text align="left">Price(INR): {item.price}</Text>
+                      <Text align="left">Price(INR): ₹ {item.price}</Text>
                     </Box>
                   </CardBody>
 
