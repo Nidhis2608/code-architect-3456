af87da20e83695295862d81c5e548bc35e8080b6
anshukumari181405 <146926520+anshukumari181405@users.noreply.github.com>
2024-03-03 00:50:31 +0530

    last check

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,33 +16,42 @@ import {
   IconButton,
   useMediaQuery,
 } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaUserInjured, FaBars } from 'react-icons/fa';
- import {useDispatch, useSelector} from 'react-redux';
- import { userLogout } from '../Redux/action';
+import { useDispatch, useSelector } from 'react-redux';
+import { userLogout } from '../Redux/action';
 import BONVOYAGELOGO from '../images/Foggy_logo.jpg';
 
 export const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [isSmallerThan768] = useMediaQuery('(max-width: 767px)');
-  const users=useSelector(store=>store.users)
-  const isAuth=useSelector(store=>store.isAuth)
-  const token=useSelector(store=>store.token)
-  console.log(users,isAuth,token)
+  const users = useSelector((store) => store.users);
+  const isAuth = useSelector((store) => store.isAuth);
+  const token = useSelector((store) => store.token);
+  const location = useLocation();
 
-const dispatch=useDispatch()
-const handleLogout=()=>{
-  dispatch(userLogout)
-}
+  const [activeSection, setActiveSection] = useState(location.pathname);
 
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(userLogout);
+  };
 
   return (
-    <Box style={{position:"sticky", width: '100%', zIndex: 1,backgroundColor: '#159895', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', padding: '10px 20px' }}>
+    <Box
+      style={{
+        position: 'sticky',
+        width: '100%',
+        zIndex: 1,
+        backgroundColor: '#159895',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        padding: '10px 20px',
+      }}
+    >
       <Flex maxWidth='container.xl' alignItems='center' justify='space-between'>
         <Box>
-          <Heading size='md' ml="20" style={{ fontWeight: 'bold', fontSize: '1.5rem'}}>
-            {/* LOGO */}
-            <img style={{height:"60px",width: '70px' }}  src={BONVOYAGELOGO} alt="" />
+          <Heading size='md' ml='20' style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
+            <img style={{ height: '60px', width: '70px' }} src={BONVOYAGELOGO} alt='' />
           </Heading>
         </Box>
 
@@ -63,7 +72,7 @@ const handleLogout=()=>{
                 aria-label='Open Menu'
                 onClick={onOpen}
                 variant='outline'
-                border="1px solid teal"
+                border='1px solid teal'
                 mr='2'
               />
               <Drawer placement='right' onClose={onClose} isOpen={isOpen}>
@@ -76,48 +85,78 @@ const handleLogout=()=>{
                       <ChakraLink
                         as={Link}
                         to='/'
-                        style={{ textDecoration: 'none', color: 'gray.600',fontWeight:'bold' }}
-                        onClick={onClose}
+                        style={{
+                          textDecoration: 'none',
+                          color: activeSection === 'white',
+                          fontWeight: 'bold',
+                          borderBottom: activeSection === '/' && '2px solid white',
+                        }}
+                        onClick={() => setActiveSection('/')}
                       >
                         Home
                       </ChakraLink>
                       <ChakraLink
                         as={Link}
                         to='/men'
-                        style={{ textDecoration: 'none', color: 'gray.600',fontWeight:'bold' }}
-                        _hover={{ color: 'white' }}
+                        style={{
+                          textDecoration: 'none',
+                          color: activeSection === 'white',
+                          fontWeight: 'bold',
+                          borderBottom: activeSection === '/men' && '2px solid white',
+                        }}
+                        onClick={() => setActiveSection('/men')}
                       >
                         Men
                       </ChakraLink>
                       <ChakraLink
                         as={Link}
-                        to='/Women'
-                        style={{ textDecoration: 'none', color: 'gray.600',fontWeight:'bold' }}
-                        onClick={onClose}
+                        to='/women'
+                        style={{
+                          textDecoration: 'none',
+                          color: activeSection === '/women' ? 'white' : 'gray.600',
+                          fontWeight: 'bold',
+                          borderBottom: activeSection === '/women' && '2px solid white',
+                        }}
+                        onClick={() => setActiveSection('/women')}
                       >
                         Women
                       </ChakraLink>
                       <ChakraLink
                         as={Link}
-                        to='/Kids'
-                        style={{ textDecoration: 'none', color: 'gray.600',fontWeight:'bold' }}
-                        onClick={onClose}
+                        to='/kids'
+                        style={{
+                          textDecoration: 'none',
+                          color: activeSection === '/kids' ? 'white' : 'gray.600',
+                          fontWeight: 'bold',
+                          borderBottom: activeSection === '/kids' && '2px solid white',
+                        }}
+                        onClick={() => setActiveSection('/kids')}
                       >
                         Kids
                       </ChakraLink>
                       <ChakraLink
                         as={Link}
                         to='/cart'
-                        style={{ textDecoration: 'none', color: 'gray.600',fontWeight:'bold' }}
-                        onClick={onClose}
+                        style={{
+                          textDecoration: 'none',
+                          color: activeSection === '/cart' ? 'white' : 'gray.600',
+                          fontWeight: 'bold',
+                          borderBottom: activeSection === '/cart' && '2px solid white',
+                        }}
+                        onClick={() => setActiveSection('/cart')}
                       >
                         Cart
                       </ChakraLink>
                       <ChakraLink
                         as={Link}
                         to='/about'
-                        style={{ textDecoration: 'none', color: 'gray.600',fontWeight:'bold' }}
-                        onClick={onClose}
+                        style={{
+                          textDecoration: 'none',
+                          color: activeSection === '/about' ? 'white' : 'gray.600',
+                          fontWeight: 'bold',
+                          borderBottom: activeSection === '/about' && '2px solid white',
+                        }}
+                        onClick={() => setActiveSection('/about')}
                       >
                         About
                       </ChakraLink>
@@ -131,110 +170,159 @@ const handleLogout=()=>{
               <ChakraLink
                 as={Link}
                 to='/'
-                style={{ textDecoration: 'none', padding: '10px', color: 'gray.600', transition: 'color 0.3s',fontWeight:'bold',fontSize:'21px' }}
-                _hover={{ color: 'white' }}
+                style={{
+                  textDecoration: 'none',
+                  padding: '10px',
+                  // color: activeSection === '/' ? 'white' : 'gray.600',
+                  color: activeSection === 'white',
+                  transition: 'color 0.3s',
+                  fontWeight: 'bold',
+                  fontSize: '21px',
+                  borderBottom: activeSection === '/' && '2px solid white',
+                }}
+                onClick={() => setActiveSection('/')}
               >
                 Home
               </ChakraLink>
               <ChakraLink
                 as={Link}
                 to='/men'
-                style={{ textDecoration: 'none', padding: '10px', color: 'gray.600', transition: 'color 0.3s',fontWeight:'bold',fontSize:'20px' }}
-                _hover={{ color: 'white' }}
+                style={{
+                  textDecoration: 'none',
+                  padding: '10px',
+                  // color: activeSection === '/men' ? 'white' : 'gray.600',
+                  color: activeSection === 'white',
+                  transition: 'color 0.3s',
+                  fontWeight: 'bold',
+                  fontSize: '20px',
+                  borderBottom: activeSection === '/men' && '2px solid white',
+                }}
+                onClick={() => setActiveSection('/men')}
               >
                 Men
               </ChakraLink>
               <ChakraLink
                 as={Link}
-                to='/Women'
-                style={{ textDecoration: 'none', padding: '10px', color: 'gray.600', transition: 'color 0.3s' ,fontWeight:'bold',fontSize:'20px'}}
-                _hover={{ color: 'white' }}
+                to='/women'
+                style={{
+                  textDecoration: 'none',
+                  padding: '10px',
+                  color: activeSection === '/women' ? 'white' : 'gray.600',
+                  transition: 'color 0.3s',
+                  fontWeight: 'bold',
+                  fontSize: '20px',
+                  borderBottom: activeSection === '/women' && '2px solid white',
+                }}
+                onClick={() => setActiveSection('/women')}
               >
                 Women
               </ChakraLink>
               <ChakraLink
                 as={Link}
-                to='/Kids'
-                style={{ textDecoration: 'none', padding: '10px', color: 'gray.600', transition: 'color 0.3s',fontWeight:'bold' ,fontSize:'20px'}}
-                _hover={{ color: 'white' }}
+                to='/kids'
+                style={{
+                  textDecoration: 'none',
+                  padding: '10px',
+                  color: activeSection === '/kids' ? 'white' : 'gray.600',
+                  transition: 'color 0.3s',
+                  fontWeight: 'bold',
+                  fontSize: '20px',
+                  borderBottom: activeSection === '/kids' && '2px solid white',
+                }}
+                onClick={() => setActiveSection('/kids')}
               >
                 Kids
               </ChakraLink>
               <ChakraLink
                 as={Link}
                 to='/cart'
-                style={{ textDecoration: 'none', padding: '10px', color: 'gray.600', transition: 'color 0.3s',fontWeight:'bold' ,fontSize:'20px'}}
-                _hover={{ color: 'white' }}
+                style={{
+                  textDecoration: 'none',
+                  padding: '10px',
+                  color: activeSection === '/cart' ? 'white' : 'gray.600',
+                  transition: 'color 0.3s',
+                  fontWeight: 'bold',
+                  fontSize: '20px',
+                  borderBottom: activeSection === '/cart' && '2px solid white',
+                }}
+                onClick={() => setActiveSection('/cart')}
               >
                 Cart
               </ChakraLink>
               <ChakraLink
                 as={Link}
-                to='/About'
-                style={{ textDecoration: 'none', padding: '10px', color: 'gray.600', transition: 'color 0.3s',fontWeight:'bold',fontSize:'20px' }}
-                _hover={{ color: 'white' }}
+                to='/about'
+                style={{
+                  textDecoration: 'none',
+                  padding: '10px',
+                  color: activeSection === '/about' ? 'white' : 'gray.600',
+                  transition: 'color 0.3s',
+                  fontWeight: 'bold',
+                  fontSize: '20px',
+                  borderBottom: activeSection === '/about' && '2px solid white',
+                }}
+                onClick={() => setActiveSection('/about')}
               >
                 About
               </ChakraLink>
             </>
           )}
 
-
-
-          {isAuth ? <div style={{display:"flex",alignItems:"center",gap:"20px"}}>
-
-        <h2>Hello,{users[0].firstName}</h2>
-        <Link to='/'> 
-        
-   <Button onClick={handleLogout}> Log out</Button>
-
-   </Link> 
-          </div> :  
-         ( 
+          {isAuth ? (
+            <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+              <h2>Hello, {users[0].firstName}</h2>
+              <Link to='/'>
+                <Button onClick={handleLogout}> Log out</Button>
+              </Link>
+            </div>
+          ) : (
             <Stack direction='row' spacing={2}>
-              <ChakraLink as={Link} to='/signup' >
-              <Button
-                leftIcon={<FaUserInjured />}
-                colorScheme='#159895'
-                variant='solid'
-                fontWeight='bold'
-                size='md'
-
-                backgroundColor= 'black'
-                color= 'white'
-            
-                _hover={{
+              <ChakraLink
+                as={Link}
+                to='/signup'
+                style={{ textDecoration: 'none' }}
+                onClick={() => setActiveSection('/signup')}
+              >
+                <Button
+                  leftIcon={<FaUserInjured />}
+                  colorScheme='#159895'
+                  variant='solid'
+                  fontWeight='bold'
+                  size='md'
+                  backgroundColor='black'
+                  color='white'
+                  _hover={{
                     backgroundColor: 'white',
                     color: 'black',
                   }}
-              >
-                SignUp
-              </Button>
+                >
+                  SignUp
+                </Button>
               </ChakraLink>
-              <ChakraLink as={Link} to='/login' >
-              <Button
-                leftIcon={<FaUserInjured />}
-                colorScheme='gray'
-                variant='solid'
-                fontWeight='bold'
-                size='md'
-                backgroundColor= 'black'
-                color= 'white'
-            
-                _hover={{
+              <ChakraLink
+                as={Link}
+                to='/login'
+                style={{ textDecoration: 'none' }}
+                onClick={() => setActiveSection('/login')}
+              >
+                <Button
+                  leftIcon={<FaUserInjured />}
+                  colorScheme='gray'
+                  variant='solid'
+                  fontWeight='bold'
+                  size='md'
+                  backgroundColor='black'
+                  color='white'
+                  _hover={{
                     backgroundColor: 'white',
                     color: 'black',
                   }}
-              >
-                Login
-              </Button>
+                >
+                  Login
+                </Button>
               </ChakraLink>
             </Stack>
-          )
-
-}
-
-
+          )}
         </Flex>
       </Flex>
     </Box>
